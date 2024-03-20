d045135389646dfc618235788bb5d2b74fed7c4f
pahomovskii <pakhomovskii@tochka.com>
2024-03-20 16:43:45 +0100

    remove debug print

--- app/handlers/handlers.py
+++ app/handlers/handlers.py
@@ -85,14 +85,8 @@ async def recommendation(request):
     try:
         company_name = request.query.get("company_name")
         records = await fetch_records(company_name)
-        print("AAAAAA")
-        print(records)
         carbon_footprints = process_records(records)
-        print("BBBBBB")
-        print(carbon_footprints)
         response_data = generate_recommendations(carbon_footprints)
-        print("CCCCCC")
-        print(response_data)
         return web.json_response({"data": response_data})
     except Exception as e:
         logger.error(f"An unexpected error occurred: {str(e)}")
